5cfd0f2c70f46a9cfdccd375535d55de34fc44dc
Simon Fessehaye <fessehay@ualberta.ca>
2020-08-23 16:05:18 -0600

    Added URL parameters

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import AddCue from "./addCue";
 import PlayButton from "./playButton";
 import AudioCue from "./audioCue";
+import { Transport, Oscillator, now, Loop } from "tone";
 
 export type AudioCue = {
   frame: number;
@@ -10,11 +11,73 @@ export type AudioCue = {
 };
 
 export default function App() {
-  const [audioCues, setAudioCues] = useState<AudioCue[]>([]);
-  const [frames, setFrames] = useState<number>(0);
+  const [audioCues, setAudioCues] = useState<AudioCue[]>([
+    { frame: 0, description: "first beat", id: Date.now() },
+  ]);
+  const [frames, setFrames] = useState<number>(60);
   const [playing, setPlaying] = useState<boolean>(false);
 
+  const osc = useRef(null);
+  const loop = useRef(null);
+
+  useEffect(() => {
+    Transport.bpm.value = 3600;
+
+    osc.current = new Oscillator(440, "sine").toDestination();
+    osc.current.volume.value = -20;
+
+    const searchParams = new URLSearchParams(window.location.search);
+    setFrames(
+      searchParams.get("frames") ? parseInt(searchParams.get("frames")) : 60
+    );
+
+    if (searchParams.get("audioCues").split(",").length > 0) {
+      setAudioCues(
+        searchParams
+          .get("audioCues")
+          .split(",")
+          .map((cue) => {
+            return { frame: parseInt(cue), description: "", id: Date.now() };
+          })
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    updateURL();
+  }, [frames, audioCues]);
+
   (window as any).state = { audioCues, frames, playing }; // for testing
+
+  const toggleLoop = () => {
+    setPlaying(!playing);
+
+    if (playing) {
+      loop.current.dispose();
+      Transport.stop();
+    } else {
+      let count = 0;
+      loop.current = new Loop((time) => {
+        // triggered every eighth note.
+        count = (count + 1) % frames;
+        if (audioCues.map((c) => c.frame).includes(count)) {
+          osc.current.start(time).stop(time + 0.002);
+        }
+      }, "4n").start(0);
+
+      Transport.start();
+    }
+  };
+
+  const updateURL = () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(
+      "audioCues",
+      audioCues.map((cue) => cue.frame.toString()).join(",")
+    );
+    searchParams.set("frames", frames.toString());
+    history.pushState(null, null, `?${searchParams.toString()}`);
+  };
 
   const addRow = () => {
     setAudioCues([...audioCues, { frame: 0, id: Date.now(), description: "" }]);
@@ -54,10 +117,7 @@ export default function App() {
 
           <div className="w-full flex items-center border-b-2 pb-4 mb-4">
             <AddCue playing={playing} addRow={addRow} />
-            <PlayButton
-              playing={playing}
-              togglePlaying={() => setPlaying(!playing)}
-            />
+            <PlayButton playing={playing} togglePlaying={() => toggleLoop()} />
           </div>
 
           {audioCues.map((audioCue: AudioCue) => {
